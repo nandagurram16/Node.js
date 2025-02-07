62d6aedf3871d624f56076067a6b1605f811d4ce
nandagurram16 <nandagurram168@gmail.com>
2025-02-07 20:28:27 +0530

    server

--- Express_js/app.js
+++ Express_js/app.js
@@ -1,6 +1,20 @@
 
-const express = require('express')
+const express = require ('express')
 
 const app = express()
 
-const port = 5500
+const port = 5000
+
+// if we want to get data from this server(app) we need to use "routr",to create router we use methods
+
+app.get('/apple',(req,res) =>{
+    res.send({
+        name:'nanda',
+        age:23,
+        place:'kadiri'
+    })
+})
+
+app.listen(port, () =>{
+    console.log('server running sucessfully')
+})
