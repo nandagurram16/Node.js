dc96676b948244b04f7ce4d2172806322ec9aaac
nandagurram16 <nandagurram168@gmail.com>
2025-02-07 17:27:56 +0530

    local_modules

---
+++ Local_modules/index.js
@@ -0,0 +1,7 @@
+
+const {addnumber,subnumber,dividenumber,multiplienumber} = require('./local')
+
+console.log(addnumber(20,40))
+console.log(subnumber(30,50))
+console.log(dividenumber(383,7474))
+console.log(multiplienumber(40,59))
---
+++ Local_modules/local.js
@@ -0,0 +1,18 @@
+
+function addnumber(a,b){
+    return a+b;
+}
+
+function subnumber(a,b){
+    return a-b
+}
+
+function dividenumber(a,b){
+    return a/b
+}
+
+function multiplienumber(a,b){
+    return a*b
+}
+
+module.exports = {addnumber,subnumber,dividenumber,multiplienumber}
